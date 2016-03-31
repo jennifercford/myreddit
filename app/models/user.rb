cd00231958dd9b0ac9071149322f19325147a5af
Jennifer Ford <jenncford@gmail.com>
2016-03-31 17:44:17 -0400

    Madd user model with corresponding route for new, a controller and a veiw, nothing came up on the page yet though

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  #validates_presence_of :password, :email
+  # validates :password, length: { minimum: 8 }
+  # validates :email, format: {
+  #             with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
+  #           }
 end
