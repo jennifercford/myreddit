fdc7a3415efb795be6c14079668f533d67ffa562
Jennifer Ford <jenncford@gmail.com>
2016-04-06 10:18:46 -0400

    now it works so only current user with comment user id same can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params["id"])
-    @post.comments.create(body: params["body"])
+    @post.comments.create(body: params["body"],
+                          user_id: current_user.id )
     redirect_to post_path(@post)
   end
 
