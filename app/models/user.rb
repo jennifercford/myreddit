00d34a78cf2fc58c83de3be55ee50d0de9f9fbc6
Jennifer Ford <jenncford@gmail.com>
2016-04-11 18:37:55 -0400

    trying to make stupid bcrypt and password digest work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :email
-  validates :password, length: { minimum: 8 }
+  
   validates :email, format: {
               with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
             }
