2dfc6e42ec83c616d97bf8f6982467b1d211e838
Jennifer Ford <jenncford@gmail.com>
2016-04-12 09:07:23 -0400

    first test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "user passwords are encrypted" do
+    password = "doodle"
+    @user = User.new(email: "test@email.com",
+    password: password)
+
+    @user.save
+    assert_not_nil @user.passsword_digest
+    assert_not =_equal password, @user.password_digest
+  end
   # test "the truth" do
   #   assert true
   # end
